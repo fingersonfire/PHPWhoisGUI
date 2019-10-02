263ed896ca0caaeed1b440892ce4eafc5e610da1
FingersOnFire <fingersonfirecom+github@gmail.com>
2019-10-02 21:07:36 +0200

    New Template System
    Uses TinyButStrong template system
    Changes error handling from PHPWhois
    Renames lib folder to vendor
    Removes style folder
    Add template folder

--- index.php
+++ index.php
@@ -1,82 +1,62 @@
 <?php
 
-include('./lib/phpwhois/whois.main.php');
+include('./vendor/phpwhois/whois.main.php');
+include_once('./vendor/tinybutstrong/tbs_class.php');
 
+function cleanupQuery($query) {
+	$queryclean = trim($query);
+	if(substr(strtolower($queryclean), 0, 7) == "http://") $queryclean = substr($query, 7);
+	if(substr(strtolower($queryclean), 0, 8) == "https://") $queryclean = substr($query, 8);
+	if(substr(strtolower($queryclean), 0, 4) == "www.") $queryclean = substr($query, 4);
+	
+	return $queryclean;
+} 
+
+// Customization
+$templatelocation = './template/default/';
+
+// Initialsation 
+$TBS = new clsTinyButStrong;
 $query = $_GET['q'];
 
-?>
-<html>
-<head>
-<title>WhoIs Lookup</title>
- <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link rel="stylesheet" type="text/css" href="./style/reset.css">
- <link rel="stylesheet" type="text/css" href="./style/simple/main.css">
-</head>
+if(isset($query) && $query != "") {
 
-<body>
-
-<div id="page-wrapper">
-
-<div class="search">
-	<form action="<?=$_SERVER['PHP_SELF'];?>">
-		<input type="text" name="q" class="searchbox" placeholder="Search" value="<?=$query;?>">
-		<input type="submit" class="searchbutton" value="&#x1F50D;">
-	</form>
-</div>
-
-
-	<?php
-	if($query) {
-		$query = trim($query);
-		if(substr(strtolower($query), 0, 7) == "http://") $query = substr($query, 7);
-		if(substr(strtolower($query), 0, 8) == "https://") $query = substr($query, 8);
-		if(substr(strtolower($query), 0, 4) == "www.") $query = substr($query, 4);
+	$query = cleanupQuery($query);	
+	$whois = new Whois();
+	$result = $whois->Lookup($query,false);
 		
-		$whois = new Whois();
-		$result = $whois->Lookup($query,false);
-		
-		echo "<div class=\"result\">\n";
-		
-			if ($whois->Query['status'] < 0){
-				echo '<pre>	'.implode($whois->Query['errstr'],"\n").'</pre>';
-			}
-			else{	
-				switch($whois->Query['tld']){
-					case 'ip':
-						echo '<pre>'.implode($result['rawdata'],"\n").'</pre>';
-						break;
-					case 'as':
-						echo '<pre>'.implode($result['rawdata'],"\n").'</pre>';
-						break;
-					default:
-						if($result['regrinfo']['registered'] == 'yes'){
-							echo '<h1>'.$result['regrinfo']['domain']['name'].'</h1>';
-							echo '<b>Registrar : '.$result['regyinfo']['registrar'].'</b><br />';
-							echo '<b>Created : '.$result['regrinfo']['domain']['created'].'</b><br />';
-							echo '<b>Changed : '.$result['regrinfo']['domain']['changed'].'</b><br />';
-							echo '<b>Name Servers : '.implode($result['regrinfo']['domain']['nserver'],"<br />\n").'</b><br />';
-						}
-						else
-							echo '<h1>THE DOMAIN '.$result['regrinfo']['domain']['name'].' IS AVAILABLE</h1>';
-				}
-			}
-		
-		echo "\n</div>\n";
-		
-		
-		if(isset($_GET['d']) && $_GET['d'] == 'debug'){
-			echo "<div class=\"result\"><pre>\n";
-			print_r($result);
-			echo "\n</pre></div>\n";
+	if ($whois->Query['status'] == 'error'){
+		$whoisError = implode($whois->Query['errstr'],"\n");
+		$TBS->LoadTemplate($templatelocation.'showerror.html');
+		$TBS->Show();
+	}
+	else{	
+		if($whois->Query['tld'] == 'ip' || $whois->Query['tld'] == 'as'){
+				$rawdata = implode($result['rawdata'],"\n");
+				$TBS->LoadTemplate($templatelocation.'showrawdata.html');
+				$TBS->Show();
 		}
-		
-		if(isset($_GET['d']) && $_GET['d'] == 'raw'){
-			echo '<div class="result"><pre>'.implode($result['rawdata'],"\n").'</pre></div>';
+		else {
+			if($result['regrinfo']['registered'] == 'yes'){
+				$domainname = $result['regrinfo']['domain']['name'];
+				$registrar = $result['regyinfo']['registrar'];
+				$domaincreation = $result['regrinfo']['domain']['created'];
+				$domainupdate = $result['regrinfo']['domain']['changed'];
+				$nameservers = implode($result['regrinfo']['domain']['nserver'],"\n");
+				$TBS->LoadTemplate($templatelocation.'showdomain.html');
+				$TBS->Show();
+			}
+			else {
+				$TBS->LoadTemplate($templatelocation.'showdomainavailable.html');
+				$TBS->Show();
+			}
 		}
 	}
-	?>
+}
+else{
+	$TBS->LoadTemplate($templatelocation.'index.html');
+	$TBS->Show();
+}
 
-</div>
-</body>
-</html>
+
+?>
